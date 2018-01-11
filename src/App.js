ad932ff77ee14c37313df0351d4b6d8cf4269d70
Angela Wilson <wilsonange@gmail.com>
2018-01-11 13:04:46 -0800

    add flip flashcard functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,103 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactCardFlip from 'react-card-flip';
+
+// Code for logged-in users
+// import { getQueryParams } from './utils';
+// import Login from './Login';
+// import Main from './Main';
+
+// test button for React TODO: consider deleting
+import Button from 'react-toolbox/lib/button/Button';
+
+//
+// class App extends Component {
+//   constructor() {
+//     super();
+//
+//     // const params = getQueryParams()
+//     // this.state = { token: params.token }
+//   }
+//
+//   render() {
+//     return (
+//       // <div className="root">
+//       //   <Button raised primary>
+//       //     Hi!
+//       //   </Button>
+//
+//
+//
+//         <div className="page-container">
+//           <div className="card">
+//             <div className="word">
+//               안녕
+//             </div>
+//           </div>
+//         </div>
+//
+//
+//       // </div>
+//     );
+//   }
+// }
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="page-container">
+        <Card front="안녕" back="hi"/>
       </div>
     );
   }
+}
+
+class Card extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      isFlipped: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({ isFlipped: !this.state.isFlipped });
+  }
+
+  render() {
+    return (
+      <ReactCardFlip isFlipped={this.state.isFlipped}>
+        <div key="front" className="card">
+          <div className="word">
+            {this.props.front}
+            <br />
+            <button onClick={this.handleClick}>Click to flip</button>
+          </div>
+        </div>
+
+        <div key="back" className="card">
+          <div className="word">
+            {this.props.back}
+            <br/>
+            <button onClick={this.handleClick}>Click to flip</button>
+          </div>
+        </div>
+      </ReactCardFlip>
+    )
+  }
+  // render() {
+  //   return (
+  //     <div className="card">
+  //       <div className="word">
+  //         {this.props.word}
+  //       </div>
+  //     </div>
+  //   )
+  // }
 }
 
 export default App;
